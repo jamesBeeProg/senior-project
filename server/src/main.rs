249af1b85e58eb5947a9efdf2746d6ac0d61d1f0
JamesBeeProg <JamesBeeProg@gmail.com>
2022-09-13 13:56:03 -0400

    Save messages to db

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,17 @@
+mod prisma;
+
 use axum::{
     extract::{
-        ws::{Message, WebSocket, WebSocketUpgrade},
+        ws::{Message, WebSocketUpgrade},
         TypedHeader,
     },
     response::IntoResponse,
     routing::get,
-    Router,
+    Extension, Router,
 };
 use futures_util::{stream::StreamExt, SinkExt};
-use std::net::SocketAddr;
+use prisma::PrismaClient;
+use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::broadcast::{self, Sender};
 use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -22,23 +25,20 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let (tx, _) = broadcast::channel(100);
+    let client = Arc::new(prisma::new_client().await.unwrap());
+
+    let (tx, _) = broadcast::channel::<String>(100);
 
     // build our application with some routes
     let app = Router::new()
-        .route(
-            "/ws",
-            get(
-                |ws: WebSocketUpgrade, user_agent: Option<TypedHeader<headers::UserAgent>>| {
-                    ws_handler(ws, user_agent, tx)
-                },
-            ),
-        )
+        .route("/ws", get(ws_handler))
         // logging so we can see whats going on
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-        );
+        )
+        .layer(Extension(client))
+        .layer(Extension(tx));
 
     // run it with hyper
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -52,40 +52,45 @@ async fn main() {
 async fn ws_handler(
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
-    tx: Sender<String>,
+    Extension(tx): Extension<Sender<String>>,
+    Extension(client): Extension<Arc<PrismaClient>>,
 ) -> impl IntoResponse {
     if let Some(TypedHeader(user_agent)) = user_agent {
         println!("`{}` connected", user_agent.as_str());
     }
 
-    ws.on_upgrade(|socket| handle_socket(socket, tx))
-}
+    ws.on_upgrade(|socket| async {
+        // By splitting we can send and receive at the same time.
+        let (mut sender, mut receiver) = socket.split();
 
-async fn handle_socket(socket: WebSocket, tx: Sender<String>) {
-    // By splitting we can send and receive at the same time.
-    let (mut sender, mut receiver) = socket.split();
-
-    let mut rx = tx.subscribe();
-    let mut send_task = tokio::spawn(async move {
-        while let Ok(msg) = rx.recv().await {
-            // In any websocket error, break loop.
-            if sender.send(Message::Text(msg)).await.is_err() {
-                break;
+        let mut rx = tx.subscribe();
+        let mut send_task = tokio::spawn(async move {
+            while let Ok(msg) = rx.recv().await {
+                // In any websocket error, break loop.
+                if sender.send(Message::Text(msg)).await.is_err() {
+                    break;
+                }
             }
-        }
-    });
+        });
 
-    // This task will receive messages from client and send them to broadcast subscribers.
-    let mut recv_task = tokio::spawn(async move {
-        while let Some(Ok(Message::Text(text))) = receiver.next().await {
-            // Add username before message.
-            let _ = tx.send(text);
-        }
-    });
+        // This task will receive messages from client and send them to broadcast subscribers.
+        let mut recv_task = tokio::spawn(async move {
+            while let Some(Ok(Message::Text(text))) = receiver.next().await {
+                client
+                    .message()
+                    .create(text.clone(), vec![])
+                    .exec()
+                    .await
+                    .unwrap();
 
-    // If any one of the tasks exit, abort the other.
-    tokio::select! {
-        _ = (&mut send_task) => recv_task.abort(),
-        _ = (&mut recv_task) => send_task.abort(),
-    };
+                tx.send(text).unwrap();
+            }
+        });
+
+        // If any one of the tasks exit, abort the other.
+        tokio::select! {
+            _ = (&mut send_task) => recv_task.abort(),
+            _ = (&mut recv_task) => send_task.abort(),
+        };
+    })
 }
