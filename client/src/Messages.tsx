127e1140a11572e045915eae187805a65c67f282
JamesBeeProg <JamesBeeProg@gmail.com>
2022-10-25 09:50:28 -0400

    Extract Messages component

---
+++ client/src/Messages.tsx
@@ -0,0 +1,50 @@
+import { FC, useEffect, useState } from 'react';
+import {
+    Avatar,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    TextField,
+} from '@mui/material';
+import { useStore } from './store';
+
+export const Messages: FC = () => {
+    const readMessages = useStore((store) => store.readMessages);
+
+    useEffect(() => {
+        readMessages();
+    }, [readMessages]);
+
+    const messages = useStore((store) => store.messages);
+    const sendMessage = useStore((store) => store.sendMessage);
+
+    const [draft, setDraft] = useState('');
+
+    return (
+        <>
+            <TextField
+                label="Message"
+                value={draft}
+                onChange={(e) => setDraft(e.target.value)}
+                onKeyDown={async (e) => {
+                    if (e.key === 'Enter') {
+                        await sendMessage(draft);
+                        setDraft('');
+                    }
+                }}
+            />
+            <List>
+                {messages.map(({ content, id }) => (
+                    <ListItem key={id}>
+                        <ListItemAvatar>
+                            <Avatar>U</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="User" secondary="some id" />
+                        <ListItemText primary={content} secondary={id} />
+                    </ListItem>
+                ))}
+            </List>
+        </>
+    );
+};
