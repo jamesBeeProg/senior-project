9ccd59aac2d297caecc68dfc313fe074bf5ce6db
JamesBeeProg <JamesBeeProg@gmail.com>
2022-10-26 13:16:07 -0400

    Switch to node server maybe

--- server/curl.sh
+++
@@ -1,5 +0,0 @@
-curl -iH "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJmb28ifQ.odyxBktu5inAbzmZ4j0339L-gBLYedYH2bbH1NctccE" \
-    -H "Content-Type: application/json" \
-    -X POST \
-    localhost:3000/users \
-    -d '{"name": "wew"}'
