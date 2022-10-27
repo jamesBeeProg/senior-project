204132caf3177ec1136ce84a1f964e86d16f034e
JamesBeeProg <JamesBeeProg@gmail.com>
2022-10-27 12:48:13 -0400

    Add suspense in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
-import { Divider, Grid } from '@mui/material';
+import { CircularProgress, Divider, Grid } from '@mui/material';
 import { FC, Suspense, useState } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import { Messages } from './Messages';
 import { Threads } from './Threads';
 
@@ -8,23 +7,20 @@ export const App: FC = () => {
     const [selected, setSelected] = useState<string>();
 
     return (
-        <ErrorBoundary fallback={<h1>Error!</h1>}>
-            <Suspense fallback={<h1>Loading...</h1>}>
-                <Grid container>
-                    <Grid item xs>
-                        <Threads
-                            selected={selected}
-                            setSelected={setSelected}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Divider orientation="vertical" />
-                    </Grid>
-                    <Grid item xs>
-                        {selected && <Messages threadId={selected} />}
-                    </Grid>
-                </Grid>
-            </Suspense>
-        </ErrorBoundary>
+        <Grid container>
+            <Grid item xs>
+                <Suspense fallback={<CircularProgress />}>
+                    <Threads selected={selected} setSelected={setSelected} />
+                </Suspense>
+            </Grid>
+            <Grid item>
+                <Divider orientation="vertical" />
+            </Grid>
+            <Grid item xs>
+                <Suspense fallback={<CircularProgress />}>
+                    {selected && <Messages threadId={selected} />}
+                </Suspense>
+            </Grid>
+        </Grid>
     );
 };
