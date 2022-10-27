d3af5b0b19e900e2860aa1081d532db42b3d9e0d
JamesBeeProg <JamesBeeProg@gmail.com>
2022-10-27 09:10:59 -0400

    Add createThread to client

--- client/src/Threads.tsx
+++ client/src/Threads.tsx
@@ -1,14 +1,35 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { trpc } from '.';
 
 export const Threads: FC = () => {
     const { data } = trpc.threads.getThreads.useQuery();
+    const { mutate } = trpc.threads.createThread.useMutation();
+
+    const [name, setName] = useState('');
 
     return (
-        <ul>
-            {data?.map((thread) => (
-                <li key={thread.id}>{thread.name}</li>
-            ))}
-        </ul>
+        <div>
+            <input
+                placeholder="Thread name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <button
+                onClick={() => {
+                    if (!name) {
+                        return;
+                    }
+
+                    mutate({ name });
+                }}
+            >
+                Create
+            </button>
+            <ul>
+                {data?.map((thread) => (
+                    <li key={thread.id}>{thread.name}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
